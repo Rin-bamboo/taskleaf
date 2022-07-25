6320b17ba5026fc152e532b77c0887830282c361
rin-bamboo <78258130+Rin-bamboo@users.noreply.github.com>
2022-07-26 00:10:08 +0900

    ユーザーCRUD処理
    一覧・詳細・編集・削除処理を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,25 +1,45 @@
 class Admin::UsersController < ApplicationController
+  
+  def index
+      @users = User.all
+  end
+
+  def show
+      @user = User.find(params[:id])
+  end
+
   def new
       @user = User.new
+  end
+  
+  def edit
+      @user = User.find(params[:id])
   end
   
   def create
       @user = User.new(user_params)
       
       if @user.save
-          redirect_to admin_user_url(@user), notice: "ユーザー「@{@user.name}」を登録しました。"
+          redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
       else
           render :new
       end
   end
-
-  def edit
+  
+  def update
+      @user = User.find(params[:id])
+      
+      if @user.update(user_params)
+          redirect_to admin_user_url(@user), notice: "ユーザーを「#{@user.name}」を更新しました。"
+      else
+          render :edit
+      end
   end
-
-  def show
-  end
-
-  def index
+  
+  def destory
+      @user = User.find(params[:id])
+      @user.destory
+      redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
   
   private
