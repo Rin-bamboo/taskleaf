b75a7904edbf1d86a9c5226a09bf62153533bb7b
rin-bamboo <78258130+Rin-bamboo@users.noreply.github.com>
2022-08-06 20:25:30 +0900

    テストコードを書く

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { 'テストを書く' }
+    description { 'RSpec & Capybara & FactoryBotを準備する' }
+    user
+  end
+end
+  
+  
---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成しておく
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
+    cntext 'ユーザーAがログインしている時' do
+      before do
+        # ユーザーAでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+      
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 作成済みのタスクの名前が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+  end
+end
+
+
