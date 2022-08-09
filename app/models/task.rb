45e0cab52af9fb2f91ce41ba305e68bca1737327
rin-bamboo <78258130+Rin-bamboo@users.noreply.github.com>
2022-08-09 10:47:44 +0900

    テストコード完成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,5 @@
 class Task < ApplicationRecord
     
-    before_validation :set_nameless_name
-    
     scope :recent, -> { order(created_at: :desc) }
     
     validates :name, presence: true, length: { maximum: 30 }
@@ -10,10 +8,7 @@ class Task < ApplicationRecord
     belongs_to :user
     
     private
-    def set_nameless_name
-        self.name = "名称未設定" if name.blank?
-    end
-    
+
     def validate_name_not_including_comma
         errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
     end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,7 +5,7 @@ describe 'タスク管理機能', type: :system do
   let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
   let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
   let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
-
+  
   before do
     # ユーザーAでログインする
     visit login_path
@@ -37,6 +37,30 @@ describe 'タスク管理機能', type: :system do
         visit task_path( task_a )
       end
       it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+  
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+      
+    before do
+      visit new_task_path
+      fill_in '名称', with: task_name
+      click_button '登録する'
+    end
+    context '新規入力画面で名称を入力したとき' do
+      let(:task_name) { '新規作成のテストを書く' }
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+    context '新規作成画面で名称を入力しなかったとき' do
+      let(:task_name) { '' }
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
+      end
     end
   end
 end
